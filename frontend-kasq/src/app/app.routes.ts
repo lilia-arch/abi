b09816bebe8af04079e951d390ea023cef34d85f
LILIA-TOVAR <liliaabigailtovarbautista14@gmail.com>
2026-07-17 17:35:21 -0600

    Actualización general del proyecto KASQ Footwear

--- frontend-kasq/src/app/app.routes.ts
+++ frontend-kasq/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistroComponent } from './registro/registro.component';
